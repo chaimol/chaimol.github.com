040c584044633ea2a9b1a271f2b97eed57676c84
chaimol <chaimol@163.com>
2026-02-02 14:48:35 +0800

    add Jin668 TM-1_T2T D5_T2T genome info

--- product/IDconvert/tools/run.bash
+++ product/IDconvert/tools/run.bash
@@ -1,3 +1,5 @@
+##这个程序是生成棉花的基因组对应id关系时使用的原始命令
+cd /share/home/chaimao1/cotton/GeneIDconvert
 IDconvertAD.bash N244 N244.pep GhN244A GhN244D
 IDconvertAD.bash TM_1_CRI TM_1_CRI.pep Gh_A Gh_D
 IDconvertAD.bash TM_1.ZJUV_2.1 TM_1.ZJUV_2.1.pep GH_A GH_D
@@ -37,3 +39,29 @@ bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q TM-1.HAU_V1.1.A -r TM_1
 bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q TM-1.HAU_V1.1.D -r TM_1_CRI.D --query-protein TM-1.HAU_V1.1.D.pep --ref-protein TM_1_CRI.D.pep -n 2 -m 85
 cat TM-1.HAU_V1.1.A.TM_1_CRI.A.xls TM-1.HAU_V1.1.D.TM_1_CRI.D.xls >TM-1.HAU_V1.1_TM_1_CRI.xls
 python3 id2json.py TM-1.HAU_V1.1_TM_1_CRI.xls TM-1.HAU_V1.1_TM_1_CRI.json HAU_V1 CRI
+
+
+#Jin668_T2T 和N244, ZM113T2T相互转换
+IDconvertAD.bash Jin668_T2T Jin668_T2T.pep Ghjin_A Ghjin_D
+
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q Jin668_T2T.A -r N244.A --query-protein Jin668_T2T.A.pep --ref-protein N244.A.pep -n 2 -m 85
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q Jin668_T2T.D -r N244.D --query-protein Jin668_T2T.D.pep --ref-protein N244.D.pep -n 2 -m 85
+cat Jin668_T2T.A.N244.A.xls Jin668_T2T.D.N244.D.xls >Jin668_T2T_N244.xls
+python3 id2json.py Jin668_T2T_N244.xls Jin668_T2T_N244.json Jin668_T2T N244
+
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q Jin668_T2T.A -r ZM113T2T.A --query-protein Jin668_T2T.A.pep --ref-protein ZM113T2T.A.pep -n 2 -m 85
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q Jin668_T2T.D -r ZM113T2T.D --query-protein ZM113T2T.D.pep --ref-protein ZM113T2T.D.pep -n 2 -m 85
+cat Jin668_T2T.A.ZM113T2T.A.xls Jin668_T2T.D.ZM113T2T.D.xls >Jin668_T2T_ZM113T2T.xls
+python3 id2json.py Jin668_T2T_ZM113T2T.xls Jin668_T2T_ZM113.json Jin668_T2T ZM113T2T
+
+
+IDconvertAD.bash TM-1_T2T TM-1-T2T.pep GhChrA GhChrD
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q TM-1_T2T.A -r N244.A --query-protein TM-1_T2T.A.pep --ref-protein N244.A.pep -n 2 -m 85
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q TM-1_T2T.D -r N244.D --query-protein TM-1_T2T.D.pep --ref-protein N244.D.pep -n 2 -m 85
+cat TM-1_T2T.A.N244.A.xls TM-1_T2T.D.N244.D.xls >TM-1_T2T_N244.xls
+python3 id2json.py TM-1_T2T_N244.xls TM-1_T2T_N244.json TM-1_T2T N244
+
+
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q D5_T2T -r N244.D --query-protein D5_T2T.pep --ref-protein N244.D.pep -n 2 -m 85
+bsub -q normal -n 24 -o r.out -e r.err IDconvert.bash -q D5_T2T -r N244.A --query-protein D5_T2T.pep --ref-protein N244.A.pep -n 2 -m 85
+
